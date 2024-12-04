270bde64a3c9904806c3ee88490b6e2e998f43b8
Kenny Doan <kdoan93@gmail.com>
2024-12-04 18:55:15 -0500

    Initial state of Native React app

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Index page</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
