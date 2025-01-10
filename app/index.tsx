9026797731ab7dd9f110b7fcd91574e9b4b68beb
Kenny Doan <kdoan93@gmail.com>
2025-01-10 16:19:19 -0500

    Updated tailwind and metro configs

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import { Link } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 
 const index = () => {
   return (
-    <View style={styles.container}>
+    <View className='flex-1 items-center justify-center bg-white'>
         <Text>Index page</Text>
         <StatusBar style='auto'/>
-        <Link href='/profile' style={styles.link} >
+        <Link href='/profile' style={{ color: 'blue' }} >
             Go to Profile
         </Link>
     </View>
@@ -16,15 +16,3 @@ const index = () => {
 }
 
 export default index
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    link: {
-        color: 'blue',
-    }
-})
